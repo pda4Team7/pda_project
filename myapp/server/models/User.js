2aab54ef4ab8910796d4ec212310c5b7ee45c4a0
jody816 <81346079+jody816@users.noreply.github.com>
2024-05-24 21:50:02 +0900

    [comment] User 스키마 주석 내용 추가

--- myapp/server/models/User.js
+++ myapp/server/models/User.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  // 유저 아이디
   nickname: {
     type: String,
     required: true,
     unique: [true, "중복되는 닉네임이 존재합니다."],
   },
+  // 비밀번호
   password: {
     type: String,
     required: [true, "비밀번호를 입력해 주세요."],
   },
+  // 열람권
   ticket: {
     type: Number,
     default: 0,
   },
+  // 열람권 사용 여부
   today_ticket: {
     type: Boolean,
     default: false,
