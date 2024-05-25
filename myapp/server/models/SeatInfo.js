0d1cd91e199118030e622914cae258e63d197ead
jody816 <81346079+jody816@users.noreply.github.com>
2024-05-25 19:21:28 +0900

    [feat] DB 콜렉션 SeatInfo 추가 ※ expire 테스트

--- myapp/server/models/SeatInfo.js
+++ myapp/server/models/SeatInfo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const seatInfoSchema = new mongoose.Schema({
   // 사용자의 ObjectId 값
-  userId: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     required: [true, "사용자 ID 값이 없습니다."],
     ref: "User",
@@ -40,6 +40,12 @@ const seatInfoSchema = new mongoose.Schema({
   seatNum: {
     type: String,
     required: true,
+  },
+  // 생성된 시간, TTL(Time-To-Live) 인덱스 적용
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    index: { expire: "20s" },
   },
 });
 
