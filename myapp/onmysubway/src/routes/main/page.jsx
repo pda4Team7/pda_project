994bfef55992dac3237ffb2f85f68a750b6f292a
colde99 <cold_egg@gmail.com>
2024-05-27 19:44:15 +0900

    [feat] mainpage layout 및 css 구현

--- myapp/onmysubway/src/routes/main/page.jsx
+++ myapp/onmysubway/src/routes/main/page.jsx
@@ -1,33 +1,62 @@
-
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
-import Row from "react-bootstrap/Row";
-import ddota from "./ddota.jpeg";
-import "./main.css";
+import {Button,Col,Container,Image,Row} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import userimg from "~/assets/user_profile.png";
+import "./page.css";
 
 export default function MainPage() {
-  const user = {
-    name: "최진양",
-  };
+    // ** 추후 Login 페이지에 user정보를 가져오기!
+    const test_user = {
+        name: "최진양",
+    };
+    const user = test_user;
+
+    // ** 추후 지하철 2가지 방면 정보를 가져오기!
+    const test_right_go = "장암"
+    const test_left_go = "석남(거북시장)"
+    const subway_direction = [test_left_go, test_right_go];
+
+    // ** 추후 7호선 전체 역 이름 정보를 가져오기!
+    const test_station_name = ['굴포천','삼산체육관','상동','부천시청'];
+    const station_name = test_station_name
+
+
   return (
-    <Container className="justify-content-md-center">
-      <Row>
-        <Col xs={3}>
-          <Image src={ddota} className="img-fluid" roundedCircle />
-        </Col>
-        <Col xs={9} className="d-flex flex flex-row align-items-center">
-          <div>
-            <div>{user.name} </div>
-            <div className="font">남은 열람권 수를 확인 해보세요.</div>
-          </div>
-          <div>
-            <Button as="input" type="submit" value="Submit" />{" "}
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div className="main-page">
+        <nav className="user-navigation-tab">
+            <section className="user-profile-group">
+                <Image src={userimg} className="user-image" roundedCircle />
+                <div>
+                    <div id="user-nav-name">{user.name} </div>
+                    <div className="user-nav-text">남은 열람권 수를 확인 해보세요.</div>
+                </div>
+                <div>
+                    <Button as="input" type="submit"
+                    value={`내 정보\n확인`}
+                    id="btn-my-info"/>{" "}
+                    <Link to="/"></Link>
+                </div>
+            </section>
+        </nav>
+        <nav className="subway-selection">
+            <section className="subway-select-group">
+                <p id="subway-select-text">
+                    내가 탄 열차의 정보를 선택 해주세요.
+                </p>
+                <Container className="subway-select-number">
+                    box
+                </Container>
+                <div className="subway-direction-group">
+                        {subway_direction.map((elem,i)=>(
+                            <div id={'sub-dir-'+i} key={'sub-dir-'+i}>{elem}</div>
+                        ))}
+                </div>
+            </section>
+            <section className="subway-destination-select-group">
+                <div></div>
+            </section>
+        </nav>
+        
+    </div>
   );
 }
