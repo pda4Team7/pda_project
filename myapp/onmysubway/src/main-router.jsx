142f98e8252ea941e928bf0ae019affde1f7704f
jody816 <81346079+jody816@users.noreply.github.com>
2024-05-28 16:18:01 +0900

    [feat] 좌석정보입력모달페이지 초기 구현

--- myapp/onmysubway/src/main-router.jsx
+++ myapp/onmysubway/src/main-router.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
-import MainPage from '~/routes/main/page';
-import LandingPage from '~/routes/landing/page';
-import LoginPage from '~/routes/login/page';
-import SignUpPage from '~/routes/signup/page';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainPage from "~/routes/main/page";
+import LandingPage from "~/routes/landing/page";
+import LoginPage from "~/routes/login/page";
+import SignUpPage from "~/routes/signup/page";
 
+import CompletePage from "~/routes/seatInfo/completePage";
+import SeatInfoPage from "~/routes/seatInfo/page";
 
 const router = createBrowserRouter([
-{
-    path: '/',
+  {
+    path: "/",
     element: <LandingPage />,
-},
-{
-    path: '/main',
+  },
+  {
+    path: "/main",
     element: <MainPage />,
-    },
-{
-    path: '/login',
+  },
+  {
+    path: "/login",
     element: <LoginPage />,
-    },
-{
-    path: '/signup',
+  },
+  {
+    path: "/signup",
     element: <SignUpPage />,
-    },
+  },
+
+  // 수정 부탁드립니다 ㅜㅜ
+  {
+    path: "/complete",
+    element: <CompletePage></CompletePage>,
+  },
+  {
+    path: "/seatInfo",
+    element: <SeatInfoPage />,
+  },
 ]);
 export default router;
